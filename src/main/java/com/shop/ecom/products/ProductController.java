7bc6a61d479b4ee5a8114c0f925d197b9ac02b39
deluxesande <deluxesande@gmail.com>
2023-12-31 13:14:55 +0300

    complete CRUD functionality

--- src/main/java/com/shop/ecom/products/ProductController.java
+++ src/main/java/com/shop/ecom/products/ProductController.java
@@ -1,7 +1,12 @@
 package com.shop.ecom.products;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
@@ -19,5 +24,23 @@ public class ProductController {
     @GetMapping
     public List<Product> getProducts() {
         return productService.getProducts();
+    }
+
+    @PostMapping
+    // Product should come from the body of the request
+    public void addNewProduct(@RequestBody Product product) {
+        productService.addNewProduct(product);
+    }
+
+    @DeleteMapping(path = "{productId}")
+    public void deleteProduct(@PathVariable Long productId) {
+        productService.deleteProduct(productId);
+    }
+
+    @PutMapping(path = "{productId}")
+    // Using the request body to update the product
+    // Instead of using RequestParam
+    public void updateProduct(@PathVariable Long productId, @RequestBody Product product) {
+        productService.updateProduct(productId, product);
     }
 }
--- src/main/java/com/shop/ecom/products/ProductService.java
+++ src/main/java/com/shop/ecom/products/ProductService.java
@@ -2,7 +2,11 @@ package com.shop.ecom.products;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import jakarta.transaction.Transactional;
+
 import java.util.List; // Add import for List
+import java.util.Optional;
 
 @Service
 public class ProductService {
@@ -15,5 +19,69 @@ public class ProductService {
 
     public List<Product> getProducts() {
         return productRepository.findAll();
+    }
+
+    public void addNewProduct(Product product) {
+        // Name of product must be unique
+        Optional<Product> productOptional = productRepository.findProductByName(product.getName());
+
+        if (productOptional.isPresent()) {
+            throw new IllegalStateException("Product already exists");
+        }
+
+        productRepository.save(product);
+    }
+
+    public void deleteProduct(Long productId) {
+        // Check if product exists
+        boolean exists = productRepository.existsById(productId);
+        if (!exists) {
+            throw new IllegalStateException("Product with id " + productId + " does not exist");
+        }
+
+        productRepository.deleteById(productId);
+    }
+
+    @Transactional
+    public void updateProduct(Long productId, Product product) {
+        // Validate if product exists
+        Product productToUpdate = productRepository.findById(productId)
+                .orElseThrow(() -> new IllegalStateException("Product with id " + productId + " does not exist"));
+
+        // update the product
+        // if the new values are not null and not equal to the old
+        if (product.getName() != null && product.getName().length() > 0
+                && !product.getName().equals(productToUpdate.getName())) {
+            productToUpdate.setName(product.getName());
+        }
+
+        if (product.getDescription() != null && product.getDescription().length() > 0
+                && !product.getDescription().equals(productToUpdate.getDescription())) {
+            productToUpdate.setDescription(product.getDescription());
+        }
+        if (product.getPrice() != null && product.getPrice() > 0
+                && !product.getPrice().equals(productToUpdate.getPrice())) {
+            productToUpdate.setPrice(product.getPrice());
+        }
+        if (product.getImageUrl() != null && product.getImageUrl().length() > 0
+                && !product.getImageUrl().equals(productToUpdate.getImageUrl())) {
+            productToUpdate.setImageUrl(product.getImageUrl());
+        }
+        if (product.getCategory() != null && product.getCategory().length() > 0
+                && !product.getCategory().equals(productToUpdate.getCategory())) {
+            productToUpdate.setCategory(product.getCategory());
+        }
+        if (product.getBrand() != null && product.getBrand().length() > 0
+                && !product.getBrand().equals(productToUpdate.getBrand())) {
+            productToUpdate.setBrand(product.getBrand());
+        }
+        if (product.getColor() != null && product.getColor().length() > 0
+                && !product.getColor().equals(productToUpdate.getColor())) {
+            productToUpdate.setColor(product.getColor());
+        }
+        if (product.getSize() != null && product.getSize().length() > 0
+                && !product.getSize().equals(productToUpdate.getSize())) {
+            productToUpdate.setSize(product.getSize());
+        }
     }
 }
